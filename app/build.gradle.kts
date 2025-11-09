9cbe0204b8bba00d864572ab76265992049321b9
Lumince <jmrjmr1997@gmail.com>
2025-11-09 12:23:12 -0500

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 10
-        versionName = "2.3.4"
+        versionName = "2.3.4.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
